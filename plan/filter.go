ac137829587fabb7243ac3811386e46747d8a6f7
Ryan Picard <ryan.picard@gresearch.co.uk>
2026-05-14 17:10:18 +0100

    PR comments

--- plan/filter.go
+++ plan/filter.go
@@ -1,4 +1,4 @@
-// Copyright 2020 G-Research Limited
+// Copyright 2026 G-Research Limited
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
